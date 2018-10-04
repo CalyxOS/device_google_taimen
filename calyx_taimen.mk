e12e75bcd15abfe75f41bba924294c6051102218
Chirayu Desai <chirayudesai1@gmail.com>
2018-10-04 16:42:48 +0530

    Add Calyx product makefiles

---
+++ calyx_taimen.mk
@@ -0,0 +1,10 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_taimen
+PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_RESTRICT_VENDOR_FILES := false
