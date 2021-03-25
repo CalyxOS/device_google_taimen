fafc5ffb48d9886b32211db5a08a3ec14ac1d9cd
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-25 22:08:57 +0530

    Include CarrierConfig overlay from vendor
    Change-Id: I3faf6cbbeff2cb020752380d8c3bd3028a4fd036

--- calyx_taimen.mk
+++ calyx_taimen.mk
@@ -5,6 +5,8 @@ $(call inherit-product, vendor/calyx/config/common.mk)
 $(call inherit-product, device/google/wahoo/device-calyx.mk)
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
+DEVICE_PACKAGE_OVERLAYS += vendor/google_devices/taimen/vendor_overlay/carrier-naked/
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_taimen
 PRODUCT_MODEL := Pixel 2 XL
