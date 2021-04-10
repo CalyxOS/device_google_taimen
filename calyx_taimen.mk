9cd61c660c865fbc3e77496f6f8900da39c543b2
Michael Bestas <mkbestas@gmail.com>
2021-04-10 14:48:51 +0300

    calyx: Set PRODUCT_BRAND to `google`
    Change-Id: Iefd6a13a161b4f0ee77136f79325e543173e98f1

--- calyx_taimen.mk
+++ calyx_taimen.mk
@@ -8,6 +8,8 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_taimen
 PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_BRAND := google
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/taimen
